dda901714b38aea91060d5463617d5cd2e8a0b5c
Jimmi Elofsson <contact@xrb.se>
2022-06-15 22:39:53 +0200

    ExchangeInterface and strat from market_maker nistide backtest, work in progress

--- backtest.py
+++ backtest.py
@@ -12,7 +12,8 @@ import backtrader as bt
 # Import our strat
 import pandas as pd
 from market_maker import tanalysis
-from market_maker import macd_strat as mystrat
+
+from position_sniper import strat_new as mystrat
 
 # Create a Stratey
 class TestStrategy(bt.Strategy):
@@ -23,6 +24,9 @@ class TestStrategy(bt.Strategy):
         print('%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
+
+        mystrat.exchange = ExchangeInterface()
+
         # Keep a reference to the lines in the data[0] dataseries
         self.dataclose = self.datas[0].close
 
@@ -157,3 +161,181 @@ if __name__ == '__main__':
 
     # Plot the result
     cerebro.plot()
+
+
+class ExchangeInterface:
+    def __init__(self):
+        self.dry_run = None
+        self.symbol = None
+
+    def cancel_order(self, order):
+        self.cancel()
+
+    def cancel_all_orders(self):
+        if self.dry_run:
+            return
+
+        logger.info("Resetting current position. Canceling all existing orders.")
+        tickLog = self.get_instrument()['tickLog']
+
+        # In certain cases, a WS update might not make it through before we call this.
+        # For that reason, we grab via HTTP to ensure we grab them all.
+        orders = self.bitmex.http_open_orders()
+
+        for order in orders:
+            logger.info("Canceling: %s %d @ %.*f" % (order['side'], order['orderQty'], tickLog, order['price']))
+
+        if len(orders):
+            self.bitmex.cancel([order['orderID'] for order in orders])
+
+        sleep(settings.API_REST_INTERVAL)
+
+    def get_portfolio(self):
+        contracts = settings.CONTRACTS
+        portfolio = {}
+        for symbol in contracts:
+            position = self.bitmex.position(symbol=symbol)
+            instrument = self.bitmex.instrument(symbol=symbol)
+
+            if instrument['isQuanto']:
+                future_type = "Quanto"
+            elif instrument['isInverse']:
+                future_type = "Inverse"
+            elif not instrument['isQuanto'] and not instrument['isInverse']:
+                future_type = "Linear"
+            else:
+                raise NotImplementedError("Unknown future type; not quanto or inverse: %s" % instrument['symbol'])
+
+            if instrument['underlyingToSettleMultiplier'] is None:
+                multiplier = float(instrument['multiplier']) / float(instrument['quoteToSettleMultiplier'])
+            else:
+                multiplier = float(instrument['multiplier']) / float(instrument['underlyingToSettleMultiplier'])
+
+            portfolio[symbol] = {
+                "currentQty": float(position['currentQty']),
+                "futureType": future_type,
+                "multiplier": multiplier,
+                "markPrice": float(instrument['markPrice']),
+                "spot": float(instrument['indicativeSettlePrice'])
+            }
+
+        return portfolio
+
+    def calc_delta(self):
+        """Calculate currency delta for portfolio"""
+        portfolio = self.get_portfolio()
+        spot_delta = 0
+        mark_delta = 0
+        for symbol in portfolio:
+            item = portfolio[symbol]
+            if item['futureType'] == "Quanto":
+                spot_delta += item['currentQty'] * item['multiplier'] * item['spot']
+                mark_delta += item['currentQty'] * item['multiplier'] * item['markPrice']
+            elif item['futureType'] == "Inverse":
+                spot_delta += (item['multiplier'] / item['spot']) * item['currentQty']
+                mark_delta += (item['multiplier'] / item['markPrice']) * item['currentQty']
+            elif item['futureType'] == "Linear":
+                spot_delta += item['multiplier'] * item['currentQty']
+                mark_delta += item['multiplier'] * item['currentQty']
+        basis_delta = mark_delta - spot_delta
+        delta = {
+            "spot": spot_delta,
+            "mark_price": mark_delta,
+            "basis": basis_delta
+        }
+        return delta
+
+    def get_delta(self, symbol=None):
+        if symbol is None:
+            symbol = self.symbol
+        return self.get_position(symbol)['currentQty']
+
+    def get_instrument(self, symbol=None):
+        if symbol is None:
+            symbol = self.symbol
+        return self.bitmex.instrument(symbol)
+
+    def get_margin(self):
+        if self.dry_run:
+            return {'marginBalance': float(settings.DRY_BTC), 'availableFunds': float(settings.DRY_BTC)}
+        return self.bitmex.funds()
+
+    def get_orders(self):
+        if self.dry_run:
+            return []
+        return self.bitmex.open_orders()
+
+    def get_highest_buy(self):
+        buys = [o for o in self.get_orders() if o['side'] == 'Buy']
+        if not len(buys):
+            return {'price': -2**32}
+        highest_buy = max(buys or [], key=lambda o: o['price'])
+        return highest_buy if highest_buy else {'price': -2**32}
+
+    def get_lowest_sell(self):
+        sells = [o for o in self.get_orders() if o['side'] == 'Sell']
+        if not len(sells):
+            return {'price': 2**32}
+        lowest_sell = min(sells or [], key=lambda o: o['price'])
+        return lowest_sell if lowest_sell else {'price': 2**32}  # ought to be enough for anyone
+
+    def get_position(self, symbol=None):
+        if symbol is None:
+            symbol = self.symbol
+        return self.bitmex.position(symbol)
+
+    def get_ticker(self, symbol=None):
+        if symbol is None:
+            symbol = self.symbol
+        return self.bitmex.ticker_data(symbol)
+
+    def is_open(self):
+        """Check that websockets are still open."""
+        return not self.bitmex.ws.exited
+
+    def check_market_open(self):
+        instrument = self.get_instrument()
+        if instrument["state"] != "Open" and instrument["state"] != "Closed":
+            raise errors.MarketClosedError("The instrument %s is not open. State: %s" %
+                                           (self.symbol, instrument["state"]))
+
+    def get_avgentry(self, symbol=None):
+        if symbol is None:
+            symbol = self.symbol
+        return self.get_position(symbol)['avgEntryPrice'] 
+
+    def get_stops(self):
+        if self.dry_run:
+            return []
+        return self.bitmex.open_stops()
+
+    def check_if_orderbook_empty(self):
+        """This function checks whether the order book is empty"""
+        instrument = self.get_instrument()
+        if instrument['midPrice'] is None:
+            raise errors.MarketEmptyError("Orderbook is empty, cannot quote")
+
+    def amend_orders(self, orders):
+        if self.dry_run:
+            return orders
+        return self.bitmex.amend_orders(orders)
+
+    def create_orders(self, orders):
+        for order in orders:
+
+            if order.side == 'Buy':
+
+            {'price': price, 'orderQty': quantity, 'side': "Buy" if index < 0 else "Sell"}
+
+
+            order['clOrdID'] = self.orderIDPrefix + base64.b64encode(uuid.uuid4().bytes).decode('utf8').rstrip('=\n')
+            order['symbol'] = self.symbol
+            if self.postOnly:
+                order['execInst'] = 'ParticipateDoNotInitiate'
+            self._curl_bitmex(path='order', postdict=order, verb='POST',  rethrow_errors=True)
+
+
+    def cancel_orders(self, orders):
+        if self.dry_run:
+            return orders
+        return self.bitmex.cancel([order['orderID'] for order in orders])
